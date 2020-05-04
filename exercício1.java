8c9e9139e7886018754338874358bc73f09b0e09
Cyber-Bruno <63611000+Cyber-Bruno@users.noreply.github.com>
2020-05-04 19:19:12 -0300

    Add files via upload

---
+++ exercício1.java
@@ -0,0 +1,21 @@
+package exercícios_de_comandos_condicionais;
+
+import java.util.Scanner; 
+//Isso foi importato para poder usar o comando Scanner
+
+public class exercício1 {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		float numero;
+		
+		System.out.println("Digite um número: ");
+		numero = leitor.nextFloat();
+		 if( numero > 20 ) { System.out.println("O número "+numero+" é maior que 20!");
+
+	}
+
+}
+	}
