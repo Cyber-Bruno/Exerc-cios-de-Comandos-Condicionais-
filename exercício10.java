4cd456d90bd8469d6b5a52328e634e7629521510
Cyber-Bruno <63611000+Cyber-Bruno@users.noreply.github.com>
2020-05-05 18:18:20 -0300

    Add files via upload

---
+++ exercício10.java
@@ -0,0 +1,45 @@
+package exercícios_de_comandos_condicionais;
+
+import java.util.Scanner;
+
+public  class  exercício10 {
+
+	public  static  void  main ( String [] args ) {
+		
+		
+		try (Scanner leitor = new Scanner( System.in)) {
+			{
+			
+			String sexo;
+			String  Masculino = ( " Masculino " );
+			
+			float altura;
+			
+			
+			System.out.println( " Descubra seu peso ideal! " );
+			System.out.println( " Digite sua Altura (cm): " );
+			altura = leitor.nextFloat ();
+			
+			System.out.println( " Seu sexo é Masculino ou Feminino: " );
+			sexo = leitor.next();
+			//System. out. println(s1. equals(s2));
+			if (sexo. equalsIgnoreCase( Masculino ))
+			
+				System.out.println( " Seu peso ideal é: " + (( 72.7 * altura / 100 ) - 58 ));
+		
+			else
+				
+			System.out.println( " Seu peso ideal é: " + (( 62.1 * altura / 100 ) - 44.7 ));
+			
+}
+			}
+	}
+}
+		
+		
+
+	
+
+	
+	
+	
